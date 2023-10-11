d0979411167f6c00a04d28dcc1f29acd3dac7494
erastov-alex <erastov.alex@gmail.com>
2023-10-11 21:16:19 +0300

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "PACO-LVIS"
 PROJECT_NAME_FULL: str = "PACO-LVIS: Parts and Attributes of Common Objects - Large Vocabulary Instance Segmentation"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
